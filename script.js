90acb27daa310fce0c69191a3e1cf8e68c22117f
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-09-27 08:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
 
-  //your code here
-	  let result = '';
+  let result = '';
 
-  for (let i = 0; i < 7; i += 2) {
-    const value = obj[i][1];
-    const letter = obj[i][0];
+  for (let key = 0; key <= 6; key++) {
+    const [symbol, value] = obj[key];
     while (num >= value) {
-      result += letter;
+      result += symbol;
       num -= value;
     }
 
-    if (i < 6) {
-      const nextValue = obj[i + 2][1];
-      const nextLetter = obj[i + 2][0];
+    // Check for subtractive notation
+    if (key % 2 === 0 && key < 6) {
+      const nextKey = key + 2;
+      const [nextSymbol, nextValue] = obj[nextKey];
+
       if (num >= value - nextValue) {
-        result += nextLetter + letter;
+        result += nextSymbol + symbol;
         num -= value - nextValue;
       }
     }
@@ -32,12 +32,10 @@ function convertToRoman(num) {
 
   return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// Test cases
+console.log(convertToRoman(14)); // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
+console.log(convertToRoman(36)); // Output: XXXVI
 
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
